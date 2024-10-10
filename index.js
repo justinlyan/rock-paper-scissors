b17dc9faab0eec7ee42e6b85810ae995cf6c8941
Justin Lyan <33265378+justinlyan@users.noreply.github.com>
2024-10-09 23:32:57 -0500

    determine winner and loser for each round. play 5 rounds

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+
 function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   let random = Math.floor(Math.random() * 3);
   return choices[random];
-}
+};
 
 function getHumanChoice() {
   let choice = prompt("Rock, paper, or scissors?");
@@ -12,4 +13,42 @@ function getHumanChoice() {
     console.log("Not a valid choice.");
     getHumanChoice();
   }
-}
+};
+
+
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(computerChoice, humanChoice) {
+    let computerCap = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    let humanCap = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    if (humanChoice === "rock" && computerChoice === "scissors") {
+      console.log("You win! Rock beats Scissors");
+      humanScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+      console.log("You win! Paper beats Rock");
+      humanScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+      console.log("You win! Scissors beats Paper");
+      humanScore++;
+    } else if (humanChoice === computerChoice) {
+      console.log("Tie!")
+    } else {
+      console.log("You lose! " + computerCap + " beats " + humanCap);
+      computerScore++;
+    }
+  };
+
+  for (let i = 0; i < 5; i++) {
+    let computerChoice = getComputerChoice();
+    let humanChoice = getHumanChoice();
+    playRound(computerChoice, humanChoice);
+  }
+
+  console.log("You: " + humanScore);
+  console.log("Computer: " + computerScore);
+};
+
+playGame();
